95bd52a8b41b435189576a42c837b7db1cb995e2
vidyagopal <vidya2732@gmail.com>
2018-06-29 16:02:55 +0000

    Nav

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,66 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
-    return erb :index
+     erb :welcome
+     erb :index
+  end
+
+  get '/travel_location' do
+    erb :welcome
+  end
+
+  get '/weather' do
+    erb :welcome
+  end
+
+  get '/about' do
+    erb :about
+  end
+
+  get '/contact' do
+    erb :contact
+  end
+  post '/travel_location' do
+    if params[:location] == "Miami"
+      @location = "Miami"
+    elsif params[:location] == "Los Angeles"
+      @location = "Los Angeles"
+    elsif params[:location] == "New York"
+      @location = "New York"
+    elsif params[:location] == "Atlanta"
+      @location = "Atlanta"
+    else
+      @location = ""
+    end
+    erb :travel_location
+  end
+
+  post '/weather' do
+    if params[:miami_weather] == "Summer"
+      @weather = "Summer"
+      @location = "Miami"
+    elsif params[:miami_weather] == "Winter"
+      @weather = "Winter"
+      @location = "Miami"
+    elsif params[:los_angeles_weather] == "Summer"
+      @weather = "Summer"
+      @location = "Los Angeles"
+    elsif params[:los_angeles_weather] == "Winter"
+      @weather = "Winter"
+      @location = "Los Angeles"
+    elsif params[:new_york_weather] == "Summer"
+      @weather = "Summer"
+      @location = "New York"
+    elsif params[:new_york_weather] == "Winter"
+      @weather = "Winter"
+      @location = "New York"
+    elsif params[:atlanta_weather] == "Summer"
+      @weather = "Summer"
+      @location = "Atlanta"
+    elsif params[:atlanta_weather] == "Winter"
+      @weather = "Winter"
+      @location = "Atlanta"
+    end
+    erb :weather
   end
 end
--- app/models/sample_model.rb
+++ app/models/sample_model.rb
@@ -1,3 +1,98 @@
-class SampleModel
-  attr_accessor :name, :age
+def miami
+  sights = {
+  "Big Bus Miami Tour" => "See all the highlights of Miami! You can hop on and off this bus whenever you like. The top is an open double decker bus with comfortable seating.",
+  "Bike and Roll at South Beach" => "A perfect way to explore Miami. Bike along South Beach to take in the beautiful sights and see the unique Art Deco architecture the city has to offer.",
+  "Everglades Airboat Tour" => "Take a glimpse into the grasslands and swamps. Come see the Florida Gator in its natural habitat.",
+  "Kennedy Space Center" => "Experience interactive simulations, live shows, and encounters with rockets.",
+  "Jungle Island" => "A fun eco-adventure park between Downtown Miami and South Beach. You will have access to plenty of animal interaction while learning lots throughout your time here."
+  }
+  sights
+end
+
+def los_angeles
+ locations = {
+   "Disneyland" => "Coined 'The Happiest Place on Earth', Disneyland is fun for all ages. Only a short drive from LA, get ready for your day to be filled with adventure. Come and meet your childhood heros.",
+   "Hollywood Walk of Fame" => "Come walk amongst your favorite Stars! Stars are given to those in the entertainment industry within five categories: motion picture, broadcast television, music, broadcast radio, and theatre/live performance.",
+   "The Hollywood Sign" => "Hike up Mount Lee for a scenic view of the LA skyline and see the iconic Hollywood sign.",
+   "Santa Monica Pier" => "This pier is over 100 years old. This pier hosts all carnival fun, including an arcade, a ferris wheel, as well as a carousel. Visitors may also go on a free Historical Walking tour of this pier every Saturday and Sunday.",
+   "Griffith Observatory" => "With such unique architecture and beautiful sights, it's no wonder thousands of visitors come every year to view and explore this place. Visitors can look through telescopes and explore exhibits."
+ }
+ locations
+end
+
+def new_york
+  places = {
+    "Empire State Building" => "Located in the heart of New York City, the Empire State Building offers a 360-degree view, as well as an immersive experience inside this world famous landmark.",
+    "Metropolitan Museum of Art" => "New York's most visited museum and attraction. The Met presents over 5,000 years of art from every corner of the world for everyone to experience and enjoy.",
+    "Statue of Liberty and Ellis Island" => "Experience the beauty and history of the Statue of Liberty National Monument and the Ellis Island Immigration Museum, located in the New York Harbor. Explore the grounds of Liberty and Ellis Islands, stand in the shadow of the Statue of Liberty, and explore the rich history of immigration at what was once the nation's busiest immigrant inspection station.",
+    "Guggenheim Museum" => " Walk with wonder through Frank Lloyd Wright's masterpiece of modern architecture. The museum presents intriguing special exhibitions and is home to one of the world's finest collections of the 20th century.",
+    "One World Observatory and 9/11 Memorial and Museum" => "Pay your respects to the victims of the 9/11 attacks at the 9/11 Memorial and Museum. Look at the twin reflecting pools, paying tribute to all the individuals who gave up their lives in the attacks. Examine the history and impact of the events and learn the stories of loss and recovery."
+  }
+  places
+end
+
+def atlanta
+  surroundings = {
+  "World of Coca-Cola" => "Learn the story behind the world's most loved beverage brand. Get a closer than ever before to the vault containing the Coca-Cola secret formula and see the Coca-Cola bottling process. Take a trip around the world in a thrilling 4D movie experience and tempt your taste buds with Coca-Cola beverages from around the world.",
+  "CNN Studio Tour" => "Get an exclusive behind-the-scenes tour of the CNN headquarters. Get an inside look at how a live broadcast is produced and sent to viewers all over the world. Learn how CNN became a worldwide leader in delivering the news.",
+  "Georgia Aquarium" => "Experience 10 million gallons of awe-inspiring wonders at the Georgia Aquarium and visit the 4D Funbelievable Theater for an incredible show. Get up close with penguins during the daily Waddle Walk.",
+  "Fernbank Museum of Natural History" => "Experience the world of the past, through dinosaurs, live animals, cultural artifacts, and hands-on science explorations. Take a walk on the wild side as you explore 75 acres of new outdoor nature adventures featuring elevated tree pods, boardwalks, interactive exhibits, and giant old-growth trees.",
+  "College Football Hall of Fame" => "Celebrate the greatest game on earth: football, through engaging and interactive experiences, in this inspiring museum."
+  }
+  surroundings
+end
+
+
+def ny_winter
+  clothing = [
+    "Winter Jacket", "Gloves", "Scarf", "Boots", "Sweaters"
+    ]
+    clothing
+end
+
+def ny_summer
+  clothes = [
+    "Sunscreen", "Sandals", "Shorts", "Tank tops", "Sunglasses"
+    ]
+    clothes
+end
+
+def mi_winter
+ shirt = [
+   "Shoes", "Jeans", "Leggings", "Beanie"  ]
+   shirt
+end
+
+def mi_summer
+  top = [
+    "Hat", "Sunscreen", "Sunglasses", "Swimsuit", "Dresses", "Rain Jacket"
+    ]
+    top
+end
+
+def la_summer
+  tank = [
+    "Tank Tops", "Shorts/Skirts", "Swimsuit", "Dresses", "Rompers", "Sandals"
+    ]
+    tank
+end
+
+def la_winter
+  short = [
+    "Light-Weight Jacket", "Leggings", "Jeans", "Blouses", "Light Flannel"
+    ]
+    short
+end
+def atl_summer
+  jean = [
+    "Tank Tops", "Shorts/Skirts", "Swimsuit", "Dresses", "Rompers", "Sandals"
+    ]
+    jean
+end
+
+def atl_winter
+  pant = [
+    "Leggings", "Boots", "Scarves", "Beanies", "Sweaters", "Gloves", "Coat"
+    ]
+    pant
 end
